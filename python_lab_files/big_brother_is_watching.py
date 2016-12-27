f4ca8963cbdf52dacfdecc55629028f9b766c3f2
Dan Steingart <dan.steingart@gmail.com>
2016-12-27 11:44:11 -0500

    speed up by only looking at last 10 hours in example

--- python_lab_files/big_brother_is_watching.py
+++ python_lab_files/big_brother_is_watching.py
@@ -8,6 +8,7 @@ from datetime import datetime as dt
 client = mc("pithy.princeton.edu")
 print client.admin.authenticate('lab', 'is_super', mechanism='SCRAM-SHA-1')
 
+
 #Find All Distinct Core DS
 dist = client.mae221.lab5_1.distinct('coreid')
 
@@ -20,45 +21,58 @@ for d in dist:
 
 print
 #You can make, instead, a list of your coreids here
-
 # dist = []
-# dist.append('1b0033000847343432313031')
-# dist.append('24003a000247353138383138')
+# dist.append('27001b000247343337373739')
+# dist.append('2f002a000247343339373536')
 
 #For each Photon
 for core in dist:
-    #Get only the data reference for that photon
-    c = client.mae221.lab5_1.find({'coreid':core}).sort('time',-1)
     
-    #Print the time of last reading
-    lr = str(dt.fromtimestamp(c[0]['time']-5*3600))
-    lc = c[0]['coreid']
-    print "Last reading: %s from %s" %(lr,lc)
+    try:
+        #Get only the data reference for that photon for the last 10 hours
+        c = client.mae221.lab5_1.find(
+            {'coreid':core,
+             'time':{'$gt':time.time()-3600*10}
+            }).sort('time',-1)
+        
+        #Print the time of last reading
+        lr = str(dt.fromtimestamp(c[0]['time']))
+        lc = c[0]['coreid']
+        print "Last reading: %s from %s" %(lr,lc)
+        
+        #Get the history for that photon
+        df = pd.DataFrame(list(c)) #Make a Dataframe
     
-    #Get the history for that photon
-    df = pd.DataFrame(list(c)) #Make a Dataframe
-
-    #At this point, you can apply a time filter like you did all semester long and zoom in on specific times.
-
-    #Make nice dates
-    df['date'] = [dt.fromtimestamp(i) for i in df['time']]
-
-
-    #plot analog input over time
+        #At this point, you can apply a time filter like you did all semester long and zoom in on specific times.
     
-    for i in range(0,6):
-        plot(df['date'],df['a%i' % i],'.')
-    title(core)
-    ylabel('analog value A0')
+        #Make nice dates
+        df['date'] = [dt.fromtimestamp(i) for i in df['time']]
     
-    #Bound the time to december    
-    xlim([datetime.date(2016, 12, 1), datetime.date(2016, 12, 30)])
-
-    #Show full analog range
-    ylim(0,4095)
-
-    #rotate xticks    
-
-    xticks(rotation='vertical')
-    showme()
-    clf()
+    
+        #plot analog input over time
+        
+        for i in range(0,6):
+            reading = 'a%i' % i
+            plot(df['date'],df[reading],label=reading)
+        title(core)
+        legend(loc="best")
+        ylabel('analog value A0')
+        
+        #Bound the time to last 10 hours
+        d1 = datetime.datetime.fromtimestamp(time.time()-3600*10)
+        d2 = datetime.datetime.fromtimestamp(time.time())
+        xlim([d1,d2])
+    
+        #Show full analog range
+        ylim(0,4095)
+    
+        #rotate xticks    
+    
+        xticks(rotation='vertical')
+        showme()
+        clf()
+    except Exception as E:
+        print E
+        print "Likely no data for ",core,"in the last 10 hours"
+        
+client.close()
